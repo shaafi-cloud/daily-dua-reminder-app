68c9ce7632ac2de9381bedf7db27687768df05f7
shaafici51 <shafiabdi104@gmail.com>
2024-07-13 20:57:47 +0300

    changes made

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth'
 import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAOCnBJqKYG0cbJlxIeWTTVrCHRVS9Mb_E",
-  authDomain: "daily-dua-reminder.firebaseapp.com",
-  projectId: "daily-dua-reminder",
-  storageBucket: "daily-dua-reminder.appspot.com",
-  messagingSenderId: "269447530870",
-  appId: "1:269447530870:web:f26703a47bae6baef95257"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
